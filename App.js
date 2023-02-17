60fcea7a7ba352d052a754d1a2c20b4f63a54724
Sean Coutinho <SeanCoutinho@my.uopeople.edu>
2023-02-17 14:58:27 +0200

    completion of light mode/dark mode in react/css

---
+++ App.js
@@ -0,0 +1,21 @@
+import React, {useState}from "react"
+import Navbar from "./components/Navbar"
+import Main from "./components/Main"
+
+export default function App() {
+    
+    const [darkMode, setDarkMode] = useState(false)
+    
+    function toggleMode() {
+        setDarkMode(prevMode => {
+            return !prevMode
+        })
+    }
+    
+    return (
+        <div className="container">
+            <Navbar darkMode={darkMode} toggleDarkMode={toggleMode}/>
+            <Main darkMode={darkMode} toggleDarkMode={toggleMode}/>
+        </div>
+    )
+}
